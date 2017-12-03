78ed5a051185ee19795cf6276f0e427bb86bdd99
joanne1i <34197398+joanne1i@users.noreply.github.com>
2017-12-02 16:11:53 -0800

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <string>
+#include <cstddef>
+#include "node.h"
+
+using namespace std;
+
+// Constructor
+Node::Node(operator_type op, Node* operand1, Node* operand2, Node* parent) {
+    data.op = op;
+    node_t = EXPRESSION;
+    this->parent = parent;
+    this->operand1 = operand1;
+    this->operand2 = operand2;
+}
+
+// Destructor
+Node::~Node() {
+    delete parent;
+    delete operand1;
+    delete operand2;
+}
+
+void Node::set_operand1(Node* new_operand1) {
+    this->operand1 = new_operand1;
+}
+
+void Node::set_operand2(Node* new_operand2) {
+    this->operand2 = new_operand2;
+}
+
+void Node::set_parent(Node* new_parent) {
+    this->parent = new_parent;
+}
+
+void Node::set_operator(char op) {
+    if (op == '+') {
+        data.op = PLUS;
+    } else if (op == '-') {
+        data.op = MINUS;
+    } else if (op == '*') {
+        data.op = MULT;
+    } else {
+        data.op = DIVIDE;
+    }
+    node_t = EXPRESSION;
+}
+
+void Node::set_variable(char var) {
+    data.var = var;
+    node_t = VARIABLE;
+}
+
+void Node::set_value(string val) {
+    this->data.val = atoi(val.c_str());;
+    node_t = INTEGER;
+}
+
+string Node::print_infix() const {
+    string s = "(";
+    if (operand1->node_t == VARIABLE) {
+        s += operand1->data.var;
+        s += print_operator();
+        s += operand2->int_to_string();
+    }
+    else {
+        s += operand1->print_infix();
+        s += print_operator();
+        s += operand2->print_infix();
+    }
+    s += ")";
+    return s;
+}
+
+string Node::print_prefix() const {
+    string s = "";
+    if (operand1->node_t == VARIABLE) {
+        s += print_operator();
+        s += operand1->data.var;
+        s += operand2->int_to_string();
+    } else {
+        s += print_operator();
+        s += operand1->print_prefix();
+        s += operand2->print_prefix();
+    }
+    return s;
+}
+
+string Node::print_postfix() const {
+    string s = "";
+    if (operand1->node_t == VARIABLE) {
+        s += operand1->data.var;
+        s += operand2->int_to_string();
+        s += print_operator();
+    } else {
+        s += operand1->print_postfix();
+        s += operand2->print_postfix();
+        s += print_operator();
+    }
+    return s;
+}
+
+string Node::int_to_string() const {
+    return to_string(data.val);
+}
+
+string Node::print_operator() const {
+    if (data.op == PLUS) {
+        return "+";
+    } else if (data.op == MINUS) {
+        return "-";
+    } else if (data.op == MULT) {
+        return "*";
+    } else {
+        return "/";
+    }
+}
+
+string Node::gen_to_string() const {
+    if (node_t == EXPRESSION) {
+        return this->print_operator();
+    } else if (node_t == VARIABLE) {
+        string s;
+        s.push_back(data.var);
+        return s;
+    } else {
+        return this->int_to_string();
+    }
+}
+
+
---
+++ node.h
@@ -0,0 +1,54 @@
+#ifndef NODE_H
+#define NODE_H
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+enum node_type{EXPRESSION, VARIABLE, INTEGER};
+enum operator_type{PLUS, MINUS, MULT, DIVIDE};
+
+union data_type {
+    operator_type op;
+    char var;
+    int val;
+};
+
+class Node {
+	public:
+		// Constructor
+        Node(operator_type op = PLUS, Node* operand1 = NULL, Node* operand2 = NULL, Node* parent = NULL);
+        // Destructor
+        ~Node();
+
+        // Mutators
+        void set_operand1(Node* new_operand1);
+        void set_operand2(Node* new_operand2);
+        void set_parent(Node* new_parent);
+        void set_operator(char op);
+        void set_variable(char var);
+        void set_value(string val);
+
+        // Observers
+        string print_infix() const;
+        string print_prefix() const;
+        string print_postfix() const;
+        string int_to_string() const;
+        string gen_to_string() const;
+        string print_operator() const;
+        node_type get_type() const {return node_t;}
+        Node* get_operand1() const {return operand1;}
+        Node* get_operand2() const {return operand2;}
+        Node* get_parent() const {return parent;}
+
+
+
+    private:
+        node_type node_t;
+        data_type data;
+        Node* operand1;
+        Node* operand2;
+        Node* parent;
+};
+
+#endif
